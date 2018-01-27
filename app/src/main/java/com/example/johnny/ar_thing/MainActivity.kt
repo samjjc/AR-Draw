44f3e602491de2dffb01fbeacd046ca104c43c46
derek <derek@llamabagel.ca>
2018-01-27 14:02:50 -0500

    Added boilerplate code for ARToolkit

--- app/src/main/java/com/example/johnny/ar_thing/MainActivity.kt
+++ app/src/main/java/com/example/johnny/ar_thing/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.johnny.ar_thing
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import org.artoolkit.ar.base.ARActivity
+import org.artoolkit.ar.base.rendering.ARRenderer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ARActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun supplyFrameLayout(): FrameLayout = frameLayout
+
+    override fun supplyRenderer(): ARRenderer = ARRenderer()
 }

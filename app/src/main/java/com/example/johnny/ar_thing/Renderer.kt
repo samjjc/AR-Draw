f3d7bd1441817ba96b4cccaca82e7566d99ef99d
derek <derek@llamabagel.ca>
2018-01-27 14:34:04 -0500

    Fixed missing native lib problem

---
+++ app/src/main/java/com/example/johnny/ar_thing/Renderer.kt
@@ -0,0 +1,12 @@
+package com.example.johnny.ar_thing
+
+import org.artoolkit.ar.base.rendering.ARRenderer
+
+/**
+ * Created by derek on 1/27/2018.
+ */
+class Renderer : ARRenderer() {
+    override fun configureARScene(): Boolean {
+        return super.configureARScene()
+    }
+}

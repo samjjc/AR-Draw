079b2a93c3decfbf1fecdae4e53a58fb7a85510b
derek <derek@llamabagel.ca>
2018-01-27 15:19:12 -0500

    Added boilerplate initialization code for ARCore

--- app/src/main/java/com/example/johnny/ar_thing/MainActivity.kt
+++ app/src/main/java/com/example/johnny/ar_thing/MainActivity.kt
@@ -1,13 +1,75 @@
 package com.example.johnny.ar_thing
 
+import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.core.exceptions.UnavailableApkTooOldException
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GLSurfaceView.Renderer {
+
+    private lateinit var session: Session
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize GLSurfaceView
+        with (surface) {
+            preserveEGLContextOnPause = true
+            setEGLContextClientVersion(2)
+            setEGLConfigChooser(8, 8, 8, 8, 16, 0)
+            setRenderer(this@MainActivity)
+            renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+        }
+
+        var message: String? = null
+        var exception: Exception? = null
+        try {
+            session  = Session(this)
+        } catch (e: UnavailableArcoreNotInstalledException) {
+            message = "Please install ARCore"
+            exception = e
+        } catch (e: UnavailableApkTooOldException) {
+            message = "Please update ARCore";
+            exception = e;
+        } catch (e: UnavailableSdkTooOldException) {
+            message = "Please update this app";
+            exception = e;
+        } catch (e: Exception) {
+            message = "This device does not support AR";
+            exception = e;
+        }
+
+        if (message != null) {
+            Log.e("MainActivity", "Exception creating session", exception)
+            return
+        }
+
+        val config = Config(session)
+        if (session.isSupported(config)) {
+            Log.e("MainActivity", "This device does not support AR")
+        }
+
+        session.configure(config)
     }
 
+    override fun onDrawFrame(gl: GL10?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }

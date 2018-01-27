f827eaf651ad02547a69e4acb39cc8fce84e793e
derek <derek@llamabagel.ca>
2018-01-27 14:55:57 -0500

    Replaced ARToolkit with ArCore

--- app/src/main/java/com/example/johnny/ar_thing/MainActivity.kt
+++ app/src/main/java/com/example/johnny/ar_thing/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.johnny.ar_thing
 
 import android.os.Bundle
-import android.widget.FrameLayout
-import kotlinx.android.synthetic.main.activity_main.*
-import org.artoolkit.ar.base.ARActivity
-import org.artoolkit.ar.base.rendering.ARRenderer
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : ARActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    override fun supplyFrameLayout(): FrameLayout = frameLayout
-
-    override fun supplyRenderer(): ARRenderer = Renderer()
 }
